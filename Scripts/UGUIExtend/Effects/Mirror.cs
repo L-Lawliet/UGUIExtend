0d60d427792ac8ed7b47ad70089fdfe2b11304d1
L-Lawliet <1577075604@qq.com>
2017-07-19 14:50:05 +0800

    1.Mirror组件增加Tiled的支持。

--- Scripts/UGUIExtend/Effects/Mirror.cs
+++ Scripts/UGUIExtend/Effects/Mirror.cs
@@ -127,7 +127,7 @@ namespace Waiting.UGUI.Effects
                         DrawSliced(output, count);
                         break;
                     case Image.Type.Tiled:
-                        
+                        DrawTiled(output, count);
                         break;
                     case Image.Type.Filled:
 
@@ -207,6 +207,72 @@ namespace Waiting.UGUI.Effects
                     MirrorVerts(rect, output, count, true);
                     MirrorVerts(rect, output, count * 2, false);
                     break;
+            }
+        }
+
+        /// <summary>
+        /// 绘制Tiled版
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="count"></param>
+        protected void DrawTiled(List<UIVertex> verts, int count)
+        {
+            Rect rect = graphic.GetPixelAdjustedRect();
+
+            Sprite overrideSprite = (graphic as Image).overrideSprite;
+
+            if (overrideSprite == null)
+            {
+                return;
+            }
+
+            //此处使用inner是因为Image绘制Tiled时，会把透明区域也绘制了。
+            Vector4 inner = DataUtility.GetInnerUV(overrideSprite);
+
+            float uLength = inner.z - inner.x;
+
+            float vLength = inner.w - inner.y;
+
+            float w = overrideSprite.rect.width / (graphic as Image).pixelsPerUnit;
+            float h = overrideSprite.rect.height / (graphic as Image).pixelsPerUnit;
+
+            int len = count / 3;
+
+            for (int i = 0; i < len; i++)
+            {
+                UIVertex v1 = verts[i * 3];
+                UIVertex v2 = verts[i * 3 + 1];
+                UIVertex v3 = verts[i * 3 + 2];
+
+                float centerX = GetCenter(v1.position.x, v2.position.x, v3.position.x);
+
+                float centerY = GetCenter(v1.position.y, v2.position.y, v3.position.y);
+
+                if (m_MirrorType == MirrorType.Horizontal || m_MirrorType == MirrorType.Quarter)
+                {
+                    //判断三个点的水平位置是否在偶数矩形内，如果是，则把UV坐标水平翻转
+                    if (Mathf.FloorToInt((centerX - rect.xMin) / w) % 2 == 1)
+                    {
+                        v1.uv0 = GetOverturnUV(v1.uv0, inner.x, uLength, true);
+                        v2.uv0 = GetOverturnUV(v2.uv0, inner.x, uLength, true);
+                        v3.uv0 = GetOverturnUV(v3.uv0, inner.x, uLength, true);
+                    }
+                }
+
+                if (m_MirrorType == MirrorType.Vertical || m_MirrorType == MirrorType.Quarter)
+                {
+                    //判断三个点的垂直位置是否在偶数矩形内，如果是，则把UV坐标垂直翻转
+                    if (Mathf.FloorToInt((centerY - rect.yMin) / h) % 2 == 1)
+                    {
+                        v1.uv0 = GetOverturnUV(v1.uv0, inner.y, vLength, false);
+                        v2.uv0 = GetOverturnUV(v2.uv0, inner.y, vLength, false);
+                        v3.uv0 = GetOverturnUV(v3.uv0, inner.y, vLength, false);
+                    }
+                }
+
+                verts[i * 3] = v1;
+                verts[i * 3 + 1] = v2;
+                verts[i * 3 + 2] = v3;
             }
         }
 
@@ -398,5 +464,44 @@ namespace Waiting.UGUI.Effects
             }
             return border;
         }
+
+        /// <summary>
+        /// 返回三个点的中心点
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="p3"></param>
+        /// <returns></returns>
+        protected float GetCenter(float p1, float p2, float p3)
+        {
+            float max = Mathf.Max(Mathf.Max(p1, p2), p3);
+
+            float min = Mathf.Min(Mathf.Min(p1, p2), p3);
+
+            return (max + min) / 2;
+        }
+
+        /// <summary>
+        /// 返回翻转UV坐标
+        /// </summary>
+        /// <param name="uv"></param>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="isHorizontal"></param>
+        /// <returns></returns>
+        protected Vector2 GetOverturnUV(Vector2 uv, float start, float length, bool isHorizontal = true)
+        {
+            if (isHorizontal)
+            {
+                uv.x = length - uv.x + start;
+            }
+            else
+            {
+                uv.y = length - uv.y + start;
+            }
+
+            return uv;
+        }
+
     }
 }
